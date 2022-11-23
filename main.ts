0a4845d112e066ad7177356b276b61868b5059c1
createyouGitHub <118916290+createyouGitHub@users.noreply.github.com>
2022-11-24 00:24:46 +0900

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,77 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 7) {
+        basic.showLeds(`
+            # # # # .
+            # # . . .
+            # . # . .
+            # . . # .
+            . . . . #
+            `)
+    } else if (receivedNumber == 8) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (receivedNumber == 9) {
+        basic.showLeds(`
+            . # # # #
+            . . . # #
+            . . # . #
+            . # . . #
+            # . . . .
+            `)
+    } else if (receivedNumber == 4) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (receivedNumber == 6) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (receivedNumber == 1) {
+        basic.showLeds(`
+            . . . . #
+            # . . # .
+            # . # . .
+            # # . . .
+            # # # # .
+            `)
+    } else if (receivedNumber == 2) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (receivedNumber == 3) {
+        basic.showLeds(`
+            # . . . .
+            . # . . #
+            . . # . #
+            . . . # #
+            . # # # #
+            `)
+    } else {
+        basic.showIcon(IconNames.Silly)
+    }
+})
+basic.showIcon(IconNames.Silly)
+radio.setGroup(1)
+pins.digitalWritePin(DigitalPin.P4, input.acceleration(Dimension.X))
+pins.digitalWritePin(DigitalPin.P5, input.acceleration(Dimension.Y))
+basic.forever(function () {
+    radio.sendValue("X", Math.constrain(input.acceleration(Dimension.X), -1023, 1023))
+    radio.sendValue("Y", Math.constrain(input.acceleration(Dimension.Y), -1023, 1023))
+})
