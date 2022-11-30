68ef26e5b1a3432429e4b21c1929578605465284
createyouGitHub <118916290+createyouGitHub@users.noreply.github.com>
2022-11-30 21:12:41 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -99,17 +99,17 @@ basic.forever(function () {
     -828
     ))
     radio.sendValue("左右", pins.map(
-    pins.analogReadPin(AnalogPin.P2),
-    0,
-    826,
-    -826,
-    826
-    ))
-    radio.sendValue("上下", pins.map(
     pins.analogReadPin(AnalogPin.P3),
     280,
     840,
-    -840,
-    840
+    840,
+    -840
+    ))
+    radio.sendValue("上下", pins.map(
+    pins.analogReadPin(AnalogPin.P2),
+    0,
+    826,
+    826,
+    -826
     ))
 })
