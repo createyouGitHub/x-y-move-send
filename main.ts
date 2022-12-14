a3d892638486dbc1bde376a7eee2573cae4e67dc
createyouGitHub <118916290+createyouGitHub@users.noreply.github.com>
2022-12-14 14:58:05 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,115 +1,32 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 7) {
-        basic.showLeds(`
-            # # # # .
-            # # . . .
-            # . # . .
-            # . . # .
-            . . . . #
-            `)
-        basic.pause(500)
-        basic.clearScreen()
-    } else if (receivedNumber == 8) {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
-        basic.pause(500)
-        basic.clearScreen()
-    } else if (receivedNumber == 9) {
-        basic.showLeds(`
-            . # # # #
-            . . . # #
-            . . # . #
-            . # . . #
-            # . . . .
-            `)
-        basic.pause(500)
-        basic.clearScreen()
-    } else if (receivedNumber == 4) {
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
-        basic.pause(500)
-        basic.clearScreen()
-    } else if (receivedNumber == 6) {
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
-        basic.pause(500)
-        basic.clearScreen()
-    } else if (receivedNumber == 1) {
-        basic.showLeds(`
-            . . . . #
-            # . . # .
-            # . # . .
-            # # . . .
-            # # # # .
-            `)
-        basic.pause(500)
-        basic.clearScreen()
-    } else if (receivedNumber == 2) {
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # . # . #
-            . # # # .
-            . . # . .
-            `)
-        basic.pause(500)
-        basic.clearScreen()
-    } else if (receivedNumber == 3) {
-        basic.showLeds(`
-            # . . . .
-            . # . . #
-            . . # . #
-            . . . # #
-            . # # # #
-            `)
-        basic.pause(500)
-        basic.clearScreen()
-    }
-})
 basic.showIcon(IconNames.Silly)
-radio.setGroup(1)
+radio.setGroup(255)
 basic.forever(function () {
     radio.sendValue("X", pins.map(
     pins.analogReadPin(AnalogPin.P1),
     0,
-    828,
-    828,
-    -828
+    1023,
+    1023,
+    -1023
     ))
     radio.sendValue("Y", pins.map(
     pins.analogReadPin(AnalogPin.P0),
     0,
-    828,
-    828,
-    -828
+    1023,
+    1023,
+    -1023
     ))
-    radio.sendValue("左右", pins.map(
+    radio.sendValue("AD", pins.map(
     pins.analogReadPin(AnalogPin.P3),
-    280,
-    840,
-    840,
-    -840
+    288,
+    1022,
+    734,
+    -734
     ))
-    radio.sendValue("上下", pins.map(
+    radio.sendValue("WS", pins.map(
     pins.analogReadPin(AnalogPin.P2),
     0,
-    826,
-    826,
-    -826
+    1023,
+    1023,
+    -1023
     ))
 })
